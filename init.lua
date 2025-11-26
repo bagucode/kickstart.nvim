af48995da5a804ebd7936207bf8a48a813f9816c
Markus Gustavsson <markusgustavsson@mmbp3.local>
2025-11-26 12:19:02 +0100

    Add lazygit.nvim

--- init.lua
+++ init.lua
@@ -965,6 +965,13 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'kdheepak/lazygit.nvim',
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
